9840c082d74688ba8741b4a136caf5bae99da81d
raha96 <76937315+raha96@users.noreply.github.com>
2023-01-28 12:12:56 +0330

    Update setup.sh
    Fixed networkx version for compatibility (AttributeError: module 'networkx' has no attribute 'from_scipy_sparse_matrix' error)

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ ln -s MuxLink/pytorch_DGCNN pytorch_DGCNN
 cd pytorch_DGCNN/lib
 make -j4
 cd ../..
-pip install numpy scipy networkx tqdm sklearn gensim
+pip install numpy scipy networkx==2.0 tqdm sklearn gensim
