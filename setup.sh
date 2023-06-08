1b92927845d989b2722efa221f14a039ad489764
Raha <76937315+raha96@users.noreply.github.com>
2023-06-08 16:06:51 +0330

    Update setup.sh
    Based on experience from cloud VMs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+sudo apt update
+sudo apt install g++ make libreadline-dev htop vim python-is-python3 minisat pip git -y
+pip install scikit-learn numpy scipy networkx==2.0 tqdm gensim torch
 ln -s MuxLink/pytorch_DGCNN pytorch_DGCNN
 cd pytorch_DGCNN/lib
 make -j4
 cd ../..
-pip install numpy scipy networkx==2.0 tqdm sklearn gensim
